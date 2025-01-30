605ece0b6374f0586ebfd2f525f6e00c31dd62ac
Mateo <mateoac439@gmail.com>
2025-01-30 16:58:27 -0300

    .

--- src/config/DatabaseConfig.java
+++ src/config/DatabaseConfig.java
@@ -7,7 +7,7 @@ public class DatabaseConfig {
 
     private static final String URL = "jdbc:postgresql://localhost:5433/asur";
     private static final String USER = "mateo";
-    private static final String PASSWORD = "89z16hhd";
+    private static final String PASSWORD = "*********";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
